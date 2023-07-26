fc236105de17ac1bd1c9f6f67bd37d25ed4516e3
Your Name <you@example.com>
2023-07-27 00:16:24 +0700

    clickere

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/giaodien1.css">
+    <title>Shopping Online</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="banner">
+            <?php include('inc_banner.php') ?>
+        </div>
+        <div class="menungang">
+   
+        </div>
+        <div class="slideshow">
+        </div>
+        <div class="tinmoi">
+        </div>
+        <div class="content">
+        </div>
+        <div class="copyright">
+        </div>
+    </div>
+
+</body>
+
+</html>
